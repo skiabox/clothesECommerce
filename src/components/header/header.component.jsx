91916f4447d1ea57dbf5094691bb34e36d2931f1
Stavros Kefaleas <stavros.kefaleas@gmail.com>
2020-03-05 17:51:38 +0200

    added cartIcon and cartDropdown components to our application, also implemented cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,10 +8,13 @@ import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 //We are destructuring the object we are getting from root reducer (see at the end of the file)
 //This value is null at the beginning
-const Header = ({ currentUser }) => (
+// --> destructuring the new prop coming from mapStateToProps props.currentUser
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -32,15 +35,21 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
 // state is coming from root reducer
 // so state.user is userReducer
 // so currentUser initial state value is null
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+// --> //new prop for our component called currentUser (the key inside the returned object)
+// advanced destructuring of state with nested objects
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  //currentUser: state.user.currentUser // the path after the : coms from root-reducer.js --> user.reducer.js (nested objects)
+  currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps)(Header);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import userReducer from './user/user.reducer';
+import cartReducer from './cart/cart.reducer';
 
 export default combineReducers({
-  user: userReducer
+  user: userReducer, //userReducer is an object of type {currentUser: null}
+  cart: cartReducer //cartReducer is an object of type {hidden: true}
 });
